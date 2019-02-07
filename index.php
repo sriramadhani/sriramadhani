14a8523661cb23db0d8962efbb7bf21dfeda4836
sriramadhani <47422980+sriramadhani@users.noreply.github.com>
2019-02-07 22:51:58 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<table width="337">
+
+<td width="298" height="300" align="center"> <p><strong>Nama : Sri Ramadhani</strongg></p>
+  <p><strong> Nim : 215 280 021</strong></p>
+  <p><strong>Phone: 085 349 *** *** </strong></p>
+  <p><strong>Fax  : sriramadhani244@yahoo.com </strong></p>
+  <p><strong>Sulawesi Selatan</strong></p>
+</table>
